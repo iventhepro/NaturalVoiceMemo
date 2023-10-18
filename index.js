036d3854d4abce8b59b718e9121032c75d37cd59
Burn Levi <100278990+e031000@users.noreply.github.com>
2023-10-18 14:25:46 +0200

    send audio voice

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const tectalicOpenai = require('@tectalic/openai').default;
-
-tectalicOpenai("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")
-  .chatCompletions.create({
-    model: 'gpt-4',
-    messages: [{ role: 'user', content: 'Will using a well designed and supported third party package save time?' }]
-  })
-  .then((response) => {
-    console.log(response.data.choices[0].message.content.trim());
-  });
-
-document.getElementById("openai").addEventListener("click", main());
-
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
-
-
-
-  console.log(completion.choices[0]);
-}
-
-
-
-main();
 
 async function cloneVoice(voice_name, audioFile) {
   const fetch = require('node-fetch');
@@ -82,6 +55,11 @@ document.getElementById('stopRecording').addEventListener('click', async () => {
     mediaRecorder.stop();
   }
 })
+
+document.getElementById('sendAudio').addEventListener('click', ()=>{
+cloneVoice(document.getElementById('voiceName'), audioBlob);
+}
+)
 
 
 const API_KEY = "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"; // Replace with your actual API key
