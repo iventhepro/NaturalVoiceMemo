26224f79e23c79741f60348525a4a619e85acbdf
iventhepro <iven.kuder@bbzbl-it.ch>
2023-10-18 13:31:39 +0200

    chatgbt

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "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",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
- 
+const tectalicOpenai = require('@tectalic/openai').default;
+
+tectalicOpenai("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")
+  .chatCompletions.create({
+    model: 'gpt-4',
+    messages: [{ role: 'user', content: 'Will using a well designed and supported third party package save time?' }]
+  })
+  .then((response) => {
+    console.log(response.data.choices[0].message.content.trim());
+  });
+
+document.getElementById("openai").addEventListener("click", main());
 
 async function main() {
   const completion = await openai.chat.completions.create({
